153f7ea34b4af371593612c7df9ddc753037b6fa
Gobinda002 <gitesholi111@gmail.com>
2024-06-27 19:29:10 +0545

    sd

--- Admin/update_booking_status.php
+++ Admin/update_booking_status.php
@@ -1,34 +1,4 @@
 
-
-<!-- // include '../connect.php';
-
-
-// // Check if form data is received
-// if (isset($_POST['booking_id'], $_POST['status'])) {
-//     // Sanitize and validate input
-//     $booking_id = mysqli_real_escape_string($conn, $_POST['booking_id']);
-//     $status = mysqli_real_escape_string($conn, $_POST['status']);
-
-//     // Update the booking status
-//     $sql = "UPDATE bookings SET status='$status' WHERE booking_id=$booking_id";
-//     $result = mysqli_query($conn, $sql);
-
-//     // Check if update was successful
-//     if ($result) {
-//         echo "Booking status updated successfully.";
-//     } else {
-//         echo 'Error updating booking status: ' . mysqli_error($conn);
-//     }
-// }
-
-// // Close the database connection
-// mysqli_close($conn);
-
-// // Redirect back to the admin view
-// header('Location: bookings.php');
-// exit; -->
-
-
 <?php
 // Include the database connection file
 include '../connect.php';
